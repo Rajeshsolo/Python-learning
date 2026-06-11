b2375d2498fdec1652fb6cb3932ab7a66c70ae4c
Rajeshsolo <rajeshappuraj@gmail.com>
2026-06-11 21:28:38 +0530

    Update datatypes.py

---
+++ 01_Basics/datatypes.py
@@ -0,0 +1,20 @@
+# data types: data store in variable
+#    Data types- int, str, float, bool,set,tuple,dict,list
+
+name="Python"   
+age=27
+salary=27500.00
+empolyee_active=True
+list=[1,2,3,4]
+tuple=(1,2,3,4)
+dict={name:"python",age:27}
+set={1,2,3}
+
+print(name)                 
+print(age)
+print(salary)
+print(empolyee_active)
+print(list)
+print(tuple)
+print(dict)
+print(set)
